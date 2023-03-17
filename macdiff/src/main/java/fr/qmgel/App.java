7b6353d3049f00ea2f8d12fa617a124f95cfa4a9
Quentin <qmgermain@gmail.com>
2023-03-17 15:46:03 +0100

    Ajout de la structure du projet

---
+++ macdiff/src/main/java/fr/qmgel/App.java
@@ -0,0 +1,17 @@
+package fr.qmgel;
+
+/**
+ * @author Quentin G., Elodie L.
+ */
+public class App
+{
+	/**
+	 * Point d'entrée dans l'application
+	 * 
+	 * @param args les paramètres passés à l'application
+	 */
+	public static void main(String[] args)
+	{
+		//Nothing to do...
+	}
+}
---
+++ macdiff/src/test/java/fr/qmgel/AppTest.java
@@ -0,0 +1,17 @@
+package fr.qmgel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Quentin G., Elodie L.
+ */
+public class AppTest
+{
+	@Test
+	public void testMain()
+	{
+		assertTrue(true);
+	}
+}
