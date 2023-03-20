3272159669c544e941018da2ae1d4bfd1506d964
Quentin <qmgermain@gmail.com>
2023-03-20 16:17:32 +0100

    Mise à jour de la structure du projet

---
+++ macdiff/src/main/java/fr/qmgel/LearningSolver.java
@@ -0,0 +1,18 @@
+package fr.qmgel;
+
+import java.util.ArrayList;
+
+public class LearningSolver extends Solver
+{
+	public LearningSolver(Network network)
+	{
+		super(network);
+	}
+
+	public ArrayList<Solution> solve(boolean all_solutions)
+	{
+		ArrayList<Solution> solutions = new ArrayList<>();
+
+		return solutions;
+	}
+}
